ec7ae162b7399debae8a6ac7dce16b86aa3e0e4c
chogoal <goal961@naver.com>
2021-07-07 10:19:58 +0900

    test commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+# 참고 사이트 : https://daimhada.tistory.com/181
+
+def solution(n, stairs):
+    print(n)
+    print(stairs)
+    stairs_max = []
+
+
+if __name__ == '__main__':
+    stair = []
+    n = int(input())
+    stairs = [int(input()) for i in range(n)]
+
+    solution(n, stairs)
