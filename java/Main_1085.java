a7bdf1f7041803618c71f1f3052cf5dc44efd1e6
chogoal <goal961@naver.com>
2023-02-19 21:51:47 +0900

    [BOJ] 직사각형에서 탈출 / 브론즈3 / 수학

--- java/Main_1085.java
+++ java/Main_1085.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1085 { // 1085. 직사각형에서 탈출
+public class Main_1085 { // 1085. 직사각형에서 탈출 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
