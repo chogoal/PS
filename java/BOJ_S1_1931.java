ffa988ffd0a158d31b9dbe3cd35001bca234bc7e
chogoal <goal961@naver.com>
2024-01-15 12:39:18 +0900

    [BOJ] 회의실 배정

---
+++ java/BOJ_S1_1931.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_S1_1931 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] time = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            } else {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int last = 0;
+        for (int i = 0; i < N; i++) {
+            if (last <= time[i][0]) {
+                last = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
