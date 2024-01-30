391f79c2d1ca3d907457a18e7ee0631419c6edce
chogoal <goal961@naver.com>
2024-01-30 21:35:10 +0900

    [BOJ] 별 찍기 - 9

---
+++ java/BOJ_B3_2446.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B3_2446 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2 * (N - i) - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
