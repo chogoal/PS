1d47b2197901b37627b1685798b4628b9214b565
chogoal <goal961@naver.com>
2024-02-16 21:50:45 +0900

    [BOJ] 소문난 칠공주

--- java/BOJ_G3_1941.java
+++ java/BOJ_G3_1941.java
@@ -9,7 +9,7 @@ public class BOJ_G3_1941 {
     static int count;
     static char[][] seat = new char[5][5];
     static boolean[] visited = new boolean[25];
-    static int[] selected = new int[7];
+    static int[] selected = new int[7]; // 선택된 7개의 자리
 
     static int[] dx = { 0, 1, 0, -1 };
     static int[] dy = { 1, 0, -1, 0 };
@@ -34,7 +34,7 @@ public class BOJ_G3_1941 {
         if (y >= 4) return;
 
         if (cnt == 7) {
-            if (bfs()) {
+            if (bfs()) { // 7개의 자리가 선택되면, 서로 연결된 자리인지 확인
                 count++;
             }
             return;
@@ -62,24 +62,24 @@ public class BOJ_G3_1941 {
 
         boolean[] bfsVisited = new boolean[25];
 
-        Queue<int[]> queue = new ArrayDeque<int[]>();
-        queue.offer(new int[] { selected[0] / 5, selected[0] % 5 });
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        queue.offer(selected[0]);
         bfsVisited[selected[0]] = true;
         check++;
 
         while (!queue.isEmpty()) {
-            int[] top = queue.poll();
+            int top = queue.poll();
 
             for (int d = 0; d < 4; d++) {
-                int nx = top[0] + dx[d];
-                int ny = top[1] + dy[d];
+                int nx = top / 5 + dx[d];
+                int ny = top % 5 + dy[d];
                 if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
 
                 if (bfsVisited[nx * 5 + ny]) continue;
 
                 if (!visited[nx * 5 + ny]) continue;
 
-                queue.offer(new int[] { nx, ny });
+                queue.offer(nx * 5 + ny);
                 bfsVisited[nx * 5 + ny] = true;
                 check++;
             }
