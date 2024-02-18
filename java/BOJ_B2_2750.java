69f33c7bd3381ca20c0d262a58853eca4fd75959
chogoal <goal961@naver.com>
2024-02-18 18:41:29 +0900

    [BOJ] 수 정렬하기

---
+++ java/BOJ_B2_2750.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_B2_2750 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] number = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(number);
+
+        for (int n : number) {
+            sb.append(n).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
