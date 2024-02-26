f677d3c889db5b249d1040c88b14bea9501d492d
chogoal <goal961@naver.com>
2024-02-26 22:42:56 +0900

    [BOJ] 연산자 끼워넣기

--- java/BOJ_S1_14888.java
+++ java/BOJ_S1_14888.java
@@ -7,9 +7,7 @@ public class BOJ_S1_14888 {
 
     static int N;
     static int[] number;
-
-    static int[] op; // 0: 덧셈, 1: 뺄셈, 2: 곱셈, 3: 나눗셈
-    static boolean[] selected;
+    static int[] opCnt = new int[4];
 
     static int min = Integer.MAX_VALUE;
     static int max = Integer.MIN_VALUE;
@@ -20,28 +18,24 @@ public class BOJ_S1_14888 {
 
         N = Integer.parseInt(br.readLine());
         number = new int[N];
-        op = new int[N - 1];
-        selected = new boolean[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             number[i] = Integer.parseInt(st.nextToken());
         }
 
-        int index = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            int count = Integer.parseInt(st.nextToken());
-            while (count-- > 0) op[index++] = i;
+            opCnt[i] = Integer.parseInt(st.nextToken());
         }
 
-        perm(0, number[0]);
+        dfs(0, number[0]);
 
         sb.append(max).append("\n").append(min);
         System.out.println(sb.toString());
     }
 
-    private static void perm(int cnt, int value) {
+    private static void dfs(int cnt, int value) {
 
         if (cnt == N - 1) {
             min = Math.min(min, value);
@@ -49,18 +43,18 @@ public class BOJ_S1_14888 {
             return;
         }
 
-        for (int i = 1; i < N; i++) {
-            if (!selected[i]) {
-                selected[i] = true;
+        for (int i = 0; i < 4; i++) {
+            if (opCnt[i] > 0) {
+                opCnt[i]--;
 
-                switch (op[i - 1]) {
-                    case 0: perm(cnt + 1, value + number[cnt + 1]); break;
-                    case 1: perm(cnt + 1, value - number[cnt + 1]); break;
-                    case 2: perm(cnt + 1, value * number[cnt + 1]); break;
-                    case 3: perm(cnt + 1, value / number[cnt + 1]); break;
+                switch (i) {
+                    case 0: dfs(cnt + 1, value + number[cnt + 1]); break;
+                    case 1: dfs(cnt + 1, value - number[cnt + 1]); break;
+                    case 2: dfs(cnt + 1, value * number[cnt + 1]); break;
+                    case 3: dfs(cnt + 1, value / number[cnt + 1]); break;
                 }
 
-                selected[i] = false;
+                opCnt[i]++;
             }
         }
     }
