8cc3f4bfbdee4738170c22d3251ae3782fb3cc98
chogoal <goal961@naver.com>
2024-03-16 20:52:25 +0900

    [BOJ] 인구 이동

--- java/BOJ_G4_16234.java
+++ java/BOJ_G4_16234.java
@@ -9,8 +9,9 @@ public class BOJ_G4_16234 {
 
     static int N, L, R;
     static int[][] country;
-    static int[][] newCountry;
     static boolean[][] visited;
+    static int uniteSum;
+    static Queue<int[]> unite;
     static int[] dx = { 0, 1, 0, -1 };
     static int[] dy = { 1, 0, -1, 0 };
 
@@ -41,42 +42,48 @@ public class BOJ_G4_16234 {
 
     private static boolean open() {
 
-        newCountry = new int[N][N];
         visited = new boolean[N][N];
 
         // 조건을 만족하는 모든 국경선 오픈
+        boolean isMoved = false;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j);
-                }
-            }
-        }
+                    // 초기화
+                    uniteSum = 0;
+                    unite = new ArrayDeque<int[]>();
 
-        // 인구 이동이 종료된 후, 새로운 인구로 나라 갱신
-        boolean isChanged = false;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (newCountry[i][j] != 0) {
-                    country[i][j] = newCountry[i][j];
-                    isChanged = true;
+                    bfs(i, j);
+
+                    // 인구 이동
+                    if (unite.size() > 1) {
+                        isMoved = true;
+                        move(uniteSum / unite.size());
+                    }
                 }
             }
         }
 
         // 인구 이동이 없었다면, 반복 종료
-        return !isChanged;
+        return !isMoved;
+    }
+
+    private static void move(int average) {
+
+        while (!unite.isEmpty()) {
+            int[] u = unite.poll();
+            country[u[0]][u[1]] = average;
+        }
     }
 
     private static void bfs(int i, int j) {
 
         Queue<int[]> queue = new ArrayDeque<int[]>();
         queue.offer(new int[] { i, j });
-        newCountry[i][j] = -1; // 연합에 포함된 칸 체크
         visited[i][j] = true;
 
-        int uniteSum = country[i][j]; // 연합 인구 수
-        int uniteCount = 1; // 연합 칸의 개수
+        uniteSum = country[i][j]; // 연합 인구 수
+        unite.offer(new int[] { i, j });
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
@@ -90,21 +97,8 @@ public class BOJ_G4_16234 {
                 if (diff >= L && diff <= R) {
                     queue.offer(new int[] { nx, ny });
                     visited[nx][ny] = true;
-                    newCountry[nx][ny] = -1;
                     uniteSum += country[nx][ny];
-                    uniteCount++;
-                }
-            }
-        }
-
-        // 인접한 국경선 열렸다면, 인구 이동
-        int newPop = uniteSum / uniteCount;
-        if (uniteCount == 1) newPop = 0;
-
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (newCountry[r][c] == -1) {
-                    newCountry[r][c] = newPop;
+                    unite.offer(new int[] { nx, ny });
                 }
             }
         }
