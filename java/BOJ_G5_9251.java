f61a21da1ecf2acf2addfb62c6f5508787fe4844
chogoal <goal961@naver.com>
2024-04-03 21:49:31 +0900

    [BOJ] LCS

---
+++ java/BOJ_G5_9251.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_G5_9251 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        int[][] dp = new int[l1 + 1][l2 + 1];
+
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (s2.charAt(j - 1) == s1.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[l1][l2]);
+    }
+}
