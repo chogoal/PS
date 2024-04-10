0c5d306bccf39ebbfbc208991f0559ce73528f3b
chogoal <goal961@naver.com>
2024-04-11 02:04:01 +0900

    [BOJ] 과자

---
+++ java/BOJ_B4_10156.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B4_10156 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(K * N - M, 0));
+    }
+}
