69c95529fb9b37eda0fff79ab45f796265c8298f
chogoal <goal961@naver.com>
2024-04-19 17:16:41 +0900

    [BOJ] 컨베이어 벨트 위의 로봇

--- java/BOJ_G5_20055.java
+++ java/BOJ_G5_20055.java
@@ -5,8 +5,8 @@ public class BOJ_G5_20055 {
 
     static int N, K, M;
     static int[] belt;
-    static boolean[] robot;
-    static int time;
+    static Queue<Integer> queue;
+    static int time, up, down;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +17,7 @@ public class BOJ_G5_20055 {
         M = 2 * N;
 
         belt = new int[M];
-        robot = new boolean[N];
+        queue = new LinkedList<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
@@ -26,8 +26,9 @@ public class BOJ_G5_20055 {
 
         while (true) {
             time++;
+            up = (M - (time % M)) % M;
+            down = (up + (N - 1)) % M;
 
-            moveBelt();
             moveRobot();
 
             int cnt = 0;
@@ -38,49 +39,29 @@ public class BOJ_G5_20055 {
         System.out.println(time);
     }
 
-    private static void moveBelt() {
-
-        // 컨베이어 벨트 이동
-        int temp = belt[M - 1];
-        for (int i = M - 1; i > 0; i--) {
-            belt[i] = belt[i - 1];
-        }
-        belt[0] = temp;
-
-        // 로봇 이동
-        for (int i = N - 1; i > 0; i--) {
-            robot[i] = robot[i - 1];
-        }
-        robot[0] = false;
-        robot[N - 1] = false; // 내리는 위치
-    }
-
     private static void moveRobot() {
 
-        // 로봇 한 칸 이동
-        for (int i = N - 2; i >= 0; i--) {
-            if (!robot[i]) continue;
+        // 로봇 이동
+        int before = -1; // 직전에 이동한 로봇 위치
+        int size = queue.size();
 
-            int next = i + 1; // 로봇이 이동하는 다음 칸
+        while (size-- > 0) {
+            int cur = queue.poll();
+            if (cur == down) continue; // 내리는 위치에 있던 로봇
 
-            if (robot[next]) next = i; // 다음 칸에 로봇 있으면 이동x
-            else if (belt[next] == 0) next = i; // 다음 칸의 내구도가 0이면 이동x
+            int next = (cur + 1) % M;
+            if (next == before) next = cur;
+            else if (belt[next] == 0) next = cur;
 
-            // 다음 칸으로 이동할 수 있다면 이동o
-            if (next != i) {
-                robot[i] = false;
-                robot[next] = true;
-                belt[next]--;
-            }
-
-            // 다음 칸이 내리는 위치라면 로봇 없애기
-            if (next == N - 1) robot[next] = false;
+            before = next;
+            if (next != cur) belt[next]--;
+            if (next != down) queue.offer(next);
         }
 
         // 로봇 올리기
-        if (belt[0] != 0) {
-            robot[0] = true;
-            belt[0]--;
+        if (belt[up] != 0) {
+            queue.offer(up);
+            belt[up]--;
         }
     }
 }
