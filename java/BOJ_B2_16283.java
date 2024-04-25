e3f3ad3b20e7fd7bb95dbddf93825a5bcc5bb54e
chogoal <goal961@naver.com>
2024-04-25 14:43:59 +0900

    [BOJ] Farm

---
+++ java/BOJ_B2_16283.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B2_16283 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+
+        int sheep = 0;
+        for (int x = 1; x < n; x++) {
+            int y = n - x;
+            if (x + y != n) continue;
+            if (a * x + b * y != w) continue;
+
+            if (sheep == 0) sheep = x;
+            else { System.out.println(-1); return; }
+        }
+
+        if (sheep == 0) System.out.println(-1);
+        else System.out.println(sheep + " " + (n - sheep));
+    }
+}
