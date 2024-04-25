2fd7e76bbce740339b53763e09b3b6451bf8a235
chogoal <goal961@naver.com>
2024-04-25 17:41:37 +0900

    [BOJ] 통학의 신

--- java/BOJ_B3_17945.java
+++ java/BOJ_B3_17945.java
@@ -8,17 +8,12 @@ public class BOJ_B3_17945 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        if (A * A == B) System.out.println(-A);
-        else {
-            int value = (int) Math.sqrt(A * A - B);
-            sb.append(-A - value).append(" ").append(-A + value);
-
-            System.out.println(sb.toString());
-        }
+        int root = (int)Math.sqrt(A * A - B);
+        if (root == 0) System.out.println(-A);
+        else System.out.println((-A - root) + " " + (-A + root));
     }
 }
