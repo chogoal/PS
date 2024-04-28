22f2a52e50892ba3f7c7cd37889ddb74416a5c41
chogoal <goal961@naver.com>
2024-04-29 00:26:43 +0900

    [BOJ] 용감한 용사 진수

--- java/BOJ_G4_14718.java
+++ java/BOJ_G4_14718.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_G4_14718 {
@@ -13,21 +11,13 @@ public class BOJ_G4_14718 {
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
         int[][] points = new int[N][3];
-        int[] a = new int[N]; // 힘
-        int[] b = new int[N]; // 민첩
-        int[] c = new int[N]; // 지능
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             points[i][0] = Integer.parseInt(st.nextToken());
             points[i][1] = Integer.parseInt(st.nextToken());
             points[i][2] = Integer.parseInt(st.nextToken());
-
-            a[i] = points[i][0];
-            b[i] = points[i][1];
-            c[i] = points[i][2];
         }
 
         int min = 3000001;
@@ -37,12 +27,14 @@ public class BOJ_G4_14718 {
 
                     int count = 0;
                     for (int n = 0; n < N; n++) {
-                        if (points[n][0] <= a[i] && points[n][1] <= b[j] && points[n][2] <= c[k]) {
-                            count++;
-                        }
+                        if (points[n][0] <= points[i][0] &&
+                            points[n][1] <= points[j][1] &&
+                            points[n][2] <= points[k][2]) count++;
                     }
 
-                    if (count >= K) min = Math.min(min, a[i] + b[j] + c[k]);
+                    if (count >= K) {
+                        min = Math.min(min, points[i][0] + points[j][1] + points[k][2]);
+                    }
                 }
             }
         }
