6fb7ade4daf7a1c98240c9ef6111b0f4edc813cd
chogoal <goal961@naver.com>
2024-05-02 00:30:12 +0900

    [BOJ] 부등호

--- java/BOJ_S1_2529.java
+++ java/BOJ_S1_2529.java
@@ -1,82 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_S1_2529 {
 
     static int N;
-    static String[] sign;
-    static String min, max;
+    static char[] sign;
+    static boolean[] selected;
+    static List<String> answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-        sign = new String[N];
+        sign = new char[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            sign[i] = st.nextToken();
+            sign[i] = st.nextToken().charAt(0);
         }
 
-        for (int i = 0; i <= 9; i++) {
-            boolean[] selected = new boolean[10];
-            selected[i] = true;
+        selected = new boolean[10];
+        answer = new ArrayList<>();
 
-            comb(0, i + "", selected);
-        }
+        comb(-1, "");
 
-        System.out.println(max);
-        System.out.println(min);
+        sb.append(answer.get(answer.size() - 1)).append("\n").append(answer.get(0));
+        System.out.println(sb.toString());
     }
 
-    private static void comb(int n, String num, boolean[] selected) {
+    private static void comb(int n, String number) {
 
         if (n == N) {
-            findMin(num);
-            findMax(num);
+            answer.add(number);
             return;
         }
-
-        int cur = num.charAt(n) - '0';
 
         for (int i = 0; i < 10; i++) {
             if (selected[i]) continue;
 
-            if (sign[n].equals("<") && cur < i) {
-                selected[i] = true;
-                comb(n + 1, num + i, selected);
-                selected[i] = false;
-            } else if (sign[n].equals(">") && cur > i) {
-                selected[i] = true;
-                comb(n + 1, num + i, selected);
-                selected[i] = false;
+            if (n != -1) {
+                if (sign[n] == '>' && number.charAt(n) - '0' < i) continue;
+                if (sign[n] == '<' && number.charAt(n) - '0' > i) continue;
             }
-        }
-    }
 
-    private static void findMin(String num) {
-
-        if (min == null) min = num;
-        else {
-            for (int i = 0; i <= N; i++) {
-                if (min.charAt(i) == num.charAt(i)) continue;
-                if (min.charAt(i) > num.charAt(i)) min = num;
-                return;
-            }
-        }
-    }
-
-    private static void findMax(String num) {
-
-        if (max == null) max = num;
-        else {
-            for (int i = 0; i <= N; i++) {
-                if (max.charAt(i) == num.charAt(i)) continue;
-                if (max.charAt(i) < num.charAt(i)) max = num;
-                return;
-            }
+            selected[i] = true;
+            comb(n + 1, number + i);
+            selected[i] = false;
         }
     }
 }
