4b3340d2182c7557b5aba8f509b1e69905bf7ae1
chogoal <goal961@naver.com>
2024-05-02 15:59:04 +0900

    [BOJ] N과 M (8)

--- java/BOJ_S3_15657.java
+++ java/BOJ_S3_15657.java
@@ -7,8 +7,7 @@ import java.util.StringTokenizer;
 public class BOJ_S3_15657 {
 
     static int N, M;
-    static int[] array;
-    static int[] selected;
+    static int[] numbers, selected;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -17,33 +16,31 @@ public class BOJ_S3_15657 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        array = new int[N];
+        numbers = new int[N];
         selected = new int[M];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(array);
+        Arrays.sort(numbers);
 
         comb(0, 0);
 
         System.out.println(sb.toString());
     }
 
-    private static void comb(int cnt, int start) {
+    private static void comb(int cnt, int idx) {
 
         if (cnt == M) {
-            for (int s : selected) {
-                sb.append(s).append(" ");
-            }
+            for (int s : selected) sb.append(s).append(" ");
             sb.append("\n");
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            selected[cnt] = array[i];
+        for (int i = idx; i < N; i++) {
+            selected[cnt] = numbers[i];
             comb(cnt + 1, i);
         }
     }
