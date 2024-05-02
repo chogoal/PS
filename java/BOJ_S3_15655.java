6f1f5e83c86c1d5c0d580a09d63b0e0b59bb6d13
chogoal <goal961@naver.com>
2024-05-02 15:55:10 +0900

    [BOJ] N과 M (6)

---
+++ java/BOJ_S3_15655.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_15655 {
+
+    static int N, M;
+    static int[] numbers, selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        selected = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        comb(0, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void comb(int cnt, int idx) {
+
+        if (cnt == M) {
+            for (int s : selected) sb.append(s).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            selected[cnt] = numbers[i];
+            comb(cnt + 1, i + 1);
+        }
+    }
+}
