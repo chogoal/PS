70a7a1357169c35f21d9985e0019006d63cb5115
chogoal <goal961@naver.com>
2024-05-02 15:50:24 +0900

    [BOJ] N과 M (5)

---
+++ java/BOJ_S3_15654.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_15654 {
+
+    static int N, M;
+    static int[] numbers, selected;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        selected = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        perm(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void perm(int cnt) {
+
+        if (cnt == M) {
+            for (int s : selected) sb.append(s).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            selected[cnt] = numbers[i];
+            perm(cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
