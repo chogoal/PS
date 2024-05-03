90ce1a6c11a6afd0c4214ad82635d4661c521cdc
chogoal <goal961@naver.com>
2024-05-03 16:06:22 +0900

    [BOJ] N-Queen

--- java/BOJ_G4_9663.java
+++ java/BOJ_G4_9663.java
@@ -5,25 +5,24 @@ import java.io.InputStreamReader;
 public class BOJ_G4_9663 {
 
     static int N;
+    static boolean[] r, c, u, d;
     static int count;
-    static boolean[] colCheck; // 세로
-    static boolean[] diaUpCheck; // y = x
-    static boolean[] diaDownCheck; // y = -x
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        colCheck = new boolean[N];
-        diaUpCheck = new boolean[2 * N - 1];
-        diaDownCheck = new boolean[2 * N - 1];
+        r = new boolean[N];
+        c = new boolean[N];
+        u = new boolean[2 * N - 1];
+        d = new boolean[2 * N - 1];
 
         queen(0);
 
         System.out.println(count);
     }
 
-    private static void queen(int cnt) { // cnt : 가로 체크 및 퀸의 개수
+    private static void queen(int cnt) {
 
         if (cnt == N) {
             count++;
@@ -31,19 +30,17 @@ public class BOJ_G4_9663 {
         }
 
         for (int i = 0; i < N; i++) {
-            if (colCheck[i] || diaUpCheck[i + cnt] || diaDownCheck[N - 1 + i - cnt]) {
-                continue;
-            }
+            if (c[i] || u[cnt + i] || d[(N - 1) - (cnt - i)]) continue;
 
-            colCheck[i] = true;
-            diaUpCheck[i + cnt] = true;
-            diaDownCheck[N - 1 + i - cnt] = true;
+            c[i] = true;
+            u[cnt + i] = true;
+            d[(N - 1) - (cnt - i)] = true;
 
             queen(cnt + 1);
 
-            colCheck[i] = false;
-            diaUpCheck[i + cnt] = false;
-            diaDownCheck[N - 1 + i - cnt] = false;
+            c[i] = false;
+            u[cnt + i] = false;
+            d[(N - 1) - (cnt - i)] = false;
         }
     }
 }
