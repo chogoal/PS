95e5654df081affa0944334e7c5782b6fe68e6bf
chogoal <goal961@naver.com>
2024-05-07 21:50:46 +0900

    [BOJ] 부분수열의 합

--- java/BOJ_S2_1182.java
+++ java/BOJ_S2_1182.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class BOJ_S2_1182 {
 
     static int N, S;
-    static int[] numbers;
+    static int[] nums;
     static int count;
 
     public static void main(String[] args) throws IOException {
@@ -15,27 +15,26 @@ public class BOJ_S2_1182 {
 
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        numbers = new int[N];
+        nums = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
         subset(0, 0);
 
-        // S == 0이면 공집합(부분집합 중 원소 합이 0)이 되는 경우 제외해주기
         System.out.println(S == 0 ? count - 1 : count);
     }
 
-    private static void subset(int cnt, int sum) {
+    private static void subset(int cur, int sum) {
 
-        if (cnt == N) {
+        if (cur == N) {
             if (sum == S) count++;
             return;
         }
 
-        subset(cnt + 1, sum); // 현재 원소 포함하지 않는 부분집합
-        subset(cnt + 1, sum + numbers[cnt]); // 현재 원소 포함하는 부분집합
+        subset(cur + 1, sum + nums[cur]);
+        subset(cur + 1, sum);
     }
 }
