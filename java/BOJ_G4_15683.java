3f388483771c13dfbd64b370f8f1700a8c9ae223
chogoal <goal961@naver.com>
2024-05-07 19:25:24 +0900

    [BOJ] 감시

--- java/BOJ_G4_15683.java
+++ java/BOJ_G4_15683.java
@@ -2,27 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_G4_15683 {
 
     static int N, M;
-    static int min;
-    static int zero;
-    static int[][] office;
-    static ArrayList<int[]> cctv = new ArrayList<int[]>();
-
-    // 상, 하, 좌, 우
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
-    static int[][][] cctvDir =
-            {
-                { {0}, {1}, {2}, {3} },
-                { {0, 1}, {2, 3} },
-                { {0, 3}, {0, 2}, {1, 2}, {1, 3} },
-                { {0, 1, 2}, {0, 1, 3}, {0, 2, 3}, {1, 2, 3} },
-                { {0, 1, 2, 3} }
-            };
+    static int[][] map;
+    static List<int[]> cctv;
+    static int[] selected;
+    static int[][][] dir = {
+            {{ 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 }},
+            {{ 1, 1, 0, 0 }, { 0, 0, 1, 1 }},
+            {{ 1, 0, 0, 1 }, { 1, 0, 1, 0 }, { 0, 1, 0, 1 }, { 0, 1, 1, 0 }},
+            {{ 1, 0, 1, 1 }, { 1, 1, 0, 1 }, { 1, 1, 1, 0 }, { 0, 1, 1, 1 }},
+            {{ 1, 1, 1, 1 }} };
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,76 +25,97 @@ public class BOJ_G4_15683 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        office = new int[N][M];
+        map = new int[N][M];
+        cctv = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                office[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
 
-                if (office[i][j] == 0) zero++;
-                else if (office[i][j] != 6) {
-                    cctv.add(new int[] { i, j, office[i][j] });
+                if (map[i][j] >= 1 && map[i][j] <= 5) {
+                    cctv.add(new int[] { i, j, map[i][j] });
                 }
             }
         }
 
-        min = zero;
-
-        perm(0, office);
+        selected = new int[cctv.size()];
+        rotate(0);
 
         System.out.println(min);
     }
 
-    private static void perm(int cnt, int[][] office) {
+    private static void rotate(int cnt) {
 
         if (cnt == cctv.size()) {
-            min = Math.min(min, check(office));
+            min = Math.min(min, watch());
             return;
         }
 
-        int cctvType = cctv.get(cnt)[2] - 1;
-        int cctvX = cctv.get(cnt)[0];
-        int cctvY = cctv.get(cnt)[1];
+        int type = cctv.get(cnt)[2] - 1;
 
-        for (int i = 0; i < cctvDir[cctvType].length; i++) {
-
-            int[][] watch = new int[N][M];
-            for (int n = 0; n < N; n++) {
-                for (int m = 0; m < M; m++) {
-                    watch[n][m] = office[n][m];
-                }
-            }
-
-            for (int d = 0; d < cctvDir[cctvType][i].length; d++) {
-                int dir = cctvDir[cctvType][i][d];
-                int nx = cctvX + dx[dir];
-                int ny = cctvY + dy[dir];
-
-                while (true) {
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) break;
-                    if (office[nx][ny] == 6) break;
-
-                    watch[nx][ny] = -1;
-
-                    nx += dx[dir];
-                    ny += dy[dir];
-                }
-            }
-
-            perm(cnt + 1, watch);
+        for (int i = 0; i < dir[type].length; i++) {
+            selected[cnt] = i;
+            rotate(cnt + 1);
         }
     }
 
-    private static int check(int[][] watch) {
+    private static int watch() {
 
-        int count = 0;
+        int[][] mapCopy = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (watch[i][j] == 0) count++;
+                mapCopy[i][j] = map[i][j];
             }
         }
 
-        return count;
+        for (int i = 0; i < selected.length; i++) {
+            int x = cctv.get(i)[0];
+            int y = cctv.get(i)[1];
+            int t = cctv.get(i)[2] - 1;
+            int d = selected[i];
+
+            int up = dir[t][d][0];
+            int down = dir[t][d][1];
+            int left = dir[t][d][2];
+            int right = dir[t][d][3];
+
+            if (up == 1) {
+                for (int r = x; r >= 0; r--) {
+                    if (map[r][y] == 6) break;
+                    if (map[r][y] == 0) mapCopy[r][y] = -1;
+                }
+            }
+
+            if (down == 1) {
+                for (int r = x; r < N; r++) {
+                    if (map[r][y] == 6) break;
+                    if (map[r][y] == 0) mapCopy[r][y] = -1;
+                }
+            }
+
+            if (left == 1) {
+                for (int c = y; c >= 0; c--) {
+                    if (map[x][c] == 6) break;
+                    if (map[x][c] == 0) mapCopy[x][c] = -1;
+                }
+            }
+
+            if (right == 1) {
+                for (int c = y; c < M; c++) {
+                    if (map[x][c] == 6) break;
+                    if (map[x][c] == 0) mapCopy[x][c] = -1;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j= 0; j < M; j++) {
+                if (mapCopy[i][j] == 0) sum++;
+            }
+        }
+
+        return sum;
     }
 }
