9ce917e632185e7945341ea570073e6fe546964b
chogoal <goal961@naver.com>
2024-05-10 22:55:29 +0900

    [BOJ] 비숍

--- java/BOJ_G1_1799.java
+++ java/BOJ_G1_1799.java
@@ -2,68 +2,86 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_G1_1799 {
 
     static int N;
-    static int max;
-    static int[][] chess;
-    static boolean[] diaUpCheck;
-    static boolean[] diaDownCheck;
-    static ArrayList<int[]>[] list = new ArrayList[2];
+    static List<int[]> even, odd;
+    static int max, answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        chess = new int[N][N];
-        diaUpCheck = new boolean[2 * N - 1];
-        diaDownCheck = new boolean[2 * N - 1];
-
-        for (int i = 0; i < 2; i++) {
-            list[i] = new ArrayList<int[]>();
-        }
+        even = new ArrayList<>();
+        odd = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                chess[i][j] = Integer.parseInt(st.nextToken());
-                if (chess[i][j] == 1) {
-                    if ((i + j) % 2 == 0) list[0].add(new int[] { i, j });
-                    else list[1].add(new int[] { i, j });
+                if (Integer.parseInt(st.nextToken()) == 1) {
+                    if ((i + j) % 2 == 0) odd.add(new int[] { i, j });
+                    else even.add(new int[] { i, j });
                 }
             }
         }
 
         max = 0;
-        bishop(0, 0, 0);
-        int evenMax = max;
+        bishopOdd(0, 0, new boolean[2 * N - 1], new boolean[2 * N - 1]);
+        answer += max;
 
         max = 0;
-        bishop(0, 1, 0);
-        int oddMax = max;
+        bishopEven(0, 0, new boolean[2 * N - 1], new boolean[2 * N - 1]);
+        answer += max;
 
-        System.out.println(evenMax + oddMax);
+        System.out.println(answer);
     }
 
-    private static void bishop(int cnt, int color, int start) {
+    private static void bishopOdd(int cur, int cnt, boolean[] check1, boolean[] check2) {
 
-        for (int i = start; i < list[color].size(); i++) {
-            int x = list[color].get(i)[0];
-            int y = list[color].get(i)[1];
-
-            if (diaUpCheck[x + y] || diaDownCheck[x - y + N - 1]) continue;
-
-            diaUpCheck[x + y] = true;
-            diaDownCheck[x - y + N - 1] = true;
-
-            bishop(cnt + 1, color, i + 1);
-
-            diaUpCheck[x + y] = false;
-            diaDownCheck[x - y + N - 1] = false;
+        if (cur == odd.size()) {
+            max = Math.max(max, cnt);
+            return;
         }
 
-        max = Math.max(max, cnt);
+        int x = odd.get(cur)[0];
+        int y = odd.get(cur)[1];
+
+        if (check1[x + y] || check2[N - 1 - (x - y)]) {
+            bishopOdd(cur + 1, cnt, check1, check2);
+        } else {
+            check1[x + y] = true;
+            check2[N - 1 - (x - y)] = true;
+            bishopOdd(cur + 1, cnt + 1, check1, check2);
+
+            check1[x + y] = false;
+            check2[N - 1 - (x - y)] = false;
+            bishopOdd(cur + 1, cnt, check1, check2);
+        }
+    }
+
+    private static void bishopEven(int cur, int cnt, boolean[] check1, boolean[] check2) {
+
+        if (cur == even.size()) {
+            max = Math.max(max, cnt);
+            return;
+        }
+
+        int x = even.get(cur)[0];
+        int y = even.get(cur)[1];
+
+        if (check1[x + y] || check2[N - 1 - (x - y)]) {
+            bishopEven(cur + 1, cnt, check1, check2);
+        } else {
+            check1[x + y] = true;
+            check2[N - 1 - (x - y)] = true;
+            bishopEven(cur + 1, cnt + 1, check1, check2);
+
+            check1[x + y] = false;
+            check2[N - 1 - (x - y)] = false;
+            bishopEven(cur + 1, cnt, check1, check2);
+        }
     }
 }
