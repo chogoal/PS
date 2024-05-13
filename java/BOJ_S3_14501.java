f87dfa49b6c00c2c64915f8605b6547f91c96af2
chogoal <goal961@naver.com>
2024-05-13 17:24:19 +0900

    [BOJ] 퇴사

--- java/BOJ_S3_14501.java
+++ java/BOJ_S3_14501.java
@@ -5,12 +5,16 @@ import java.util.StringTokenizer;
 
 public class BOJ_S3_14501 {
 
+    static int N;
+    static int[] T, P;
+    static int max = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] T = new int[N];
-        int[] P = new int[N];
+        N = Integer.parseInt(br.readLine());
+        T = new int[N];
+        P = new int[N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,20 +22,21 @@ public class BOJ_S3_14501 {
             P[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] max = new int[N + 1];
-        for (int i = N - 1; i >= 0; i--) {
+        work(0, 0);
 
-            // 기간 내에 할 수 있는 상담인지 확인
-            if (i + T[i] > N) { max[i] = max[i + 1]; continue; }
+        System.out.println(max);
+    }
 
-            // i 일에 상담을 할 경우와 하지 않을 경우의 금액 비교
-            if (P[i] + max[i + T[i]] > max[i + 1]) {
-                max[i] = P[i] + max[i + T[i]];
-            } else {
-                max[i] = max[i + 1];
-            }
+    private static void work(int day, int profit) {
+
+        if (day > N) return;
+
+        if (day == N) {
+            max = Math.max(max, profit);
+            return;
         }
 
-        System.out.println(max[0]);
+        work(day + T[day], profit + P[day]);
+        work(day + 1, profit);
     }
 }
