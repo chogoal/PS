f6c47a801818828899ea82b0864872fc75e341c7
chogoal <goal961@naver.com>
2024-05-17 02:10:23 +0900

    [BOJ] 퇴사 2

--- java/BOJ_G5_15486.java
+++ java/BOJ_G5_15486.java
@@ -5,12 +5,16 @@ import java.util.StringTokenizer;
 
 public class BOJ_G5_15486 {
 
+    static int N;
+    static int[] T, P, dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] T = new int[N];
-        int[] P = new int[N];
+        N = Integer.parseInt(br.readLine());
+        T = new int[N];
+        P = new int[N];
+        dp = new int[N + 51];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,15 +22,24 @@ public class BOJ_G5_15486 {
             P[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] max = new long[N + 1];
-        for (int i = N - 1; i >= 0; i--) {
-
-            if (i + T[i] > N) { max[i] = max[i + 1]; continue; }
-
-            long today = P[i] + max[i + T[i]];
-            max[i] = Math.max(today, max[i + 1]);
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = Integer.MIN_VALUE;
         }
 
-        System.out.println(max[0]);
+        dp[N] = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            dp[i] = Math.max(dp[i + T[i]] + P[i], dp[i + 1]);
+        }
+
+        System.out.println(dp[0]);
     }
+
+//    private static int recur(int t) {
+//
+//        if (t > N) return Integer.MIN_VALUE;
+//        if (t == N) return 0;
+//
+//        if (dp[t] == 0) dp[t] = Math.max(recur(t + T[t]) + P[t], recur(t + 1));
+//        return dp[t];
+//    }
 }
