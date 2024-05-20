e6c81d85db00d8bddf07915535a16c94be18d913
chogoal <goal961@naver.com>
2024-05-21 02:29:20 +0900

    [BOJ] 소형기관차

--- java/BOJ_G3_2616.java
+++ java/BOJ_G3_2616.java
@@ -14,37 +14,37 @@ public class BOJ_G3_2616 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        people = new int[N];
-        dp = new int[N][3];
+        people = new int[N + 1];
+        dp = new int[N + 1][3];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             people[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
+            people[i] += people[i - 1];
+        }
+
+        for (int i = 0; i <= N; i++) {
             Arrays.fill(dp[i], -1);
         }
 
         K = Integer.parseInt(br.readLine());
 
-        System.out.println(recur(0, 0));
+        System.out.println(recur(1, 0));
     }
 
     private static int recur(int cur, int cnt) {
 
         if (cnt == 3) return 0;
 
-        if (cur > N - K) return Integer.MIN_VALUE;
+        if (cur > N + 1 - K) return Integer.MIN_VALUE;
 
         if (dp[cur][cnt] != -1) return dp[cur][cnt];
 
-        int count = 0;
-        for (int i = cur; i < cur + K; i++) {
-            count += people[i];
-        }
-
-        dp[cur][cnt] = Math.max(recur(cur + K, cnt + 1) + count, recur(cur + 1, cnt));
+        dp[cur][cnt] = Math.max(recur(cur + K, cnt + 1) + people[cur + K - 1] - people[cur - 1],
+                                recur(cur + 1, cnt));
         return dp[cur][cnt];
     }
 }
