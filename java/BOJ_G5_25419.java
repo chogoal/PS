fcefdef1f8e7afda67c053521a300b30cacda5d7
chogoal <goal961@naver.com>
2024-05-21 22:53:34 +0900

    [BOJ] 정수를 끝까지 외치자

---
+++ java/BOJ_G5_25419.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_G5_25419 {
+
+    static int N, K;
+    static boolean[] num;
+    static int[] dp;
+    static int max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        num = new boolean[N + 1];
+        dp = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            num[Integer.parseInt(st.nextToken())] = true;
+        }
+
+        Arrays.fill(dp, -1);
+
+        max = N;
+        for (int i = 0; i <= N; i++) {
+            boolean check = false;
+            for (int j = i + 1; j <= i + K; j++) {
+                if (j > N) break;
+                if (!num[j]) { check = true; break; }
+            }
+            if (!check) { max = i; break; }
+        }
+
+
+        int result = game(0);
+        System.out.println(result == 1 ? 1 : 0);
+    }
+
+    private static int game(int cur) {
+
+        if (num[cur]) return 1;
+
+        if (cur == max) return 0;
+
+        if (dp[cur] != -1) return dp[cur];
+
+        dp[cur] = 0;
+        for (int i = cur + 1; i <= cur + K; i++) {
+            if (i > N) break;
+            if (game(i) == 0) { dp[cur] = 1; break; }
+        }
+        return dp[cur];
+    }
+}
