b9fef2efa06c161e1f1e60996fb895036b5ac5f9
chogoal <goal961@naver.com>
2024-05-22 23:10:47 +0900

    [BOJ] 오븐 시계

---
+++ java/BOJ_B3_2525.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B3_2525 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+
+        A += (B + C) / 60;
+        B = (B + C) % 60;
+
+        System.out.println((A % 24) + " " + B);
+    }
+}
