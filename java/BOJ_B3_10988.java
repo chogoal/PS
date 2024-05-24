4d60697665663587fe1f1bf63086f2dad4216e58
chogoal <goal961@naver.com>
2024-05-25 00:55:03 +0900

    [BOJ] 팰린드롬인지 확인하기

---
+++ java/BOJ_B3_10988.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B3_10988 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        int check = 1;
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                check = 0;
+                break;
+            }
+        }
+
+        System.out.println(check);
+    }
+}
