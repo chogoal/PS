1a459ced79c448967027699ef2c15a4d914b5835
chogoal <goal961@naver.com>
2024-06-02 01:30:15 +0900

    [BOJ] 팩토리얼

---
+++ java/BOJ_B3_10872.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B3_10872 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int ans = 1;
+        for (int i = 1; i <= N; i++) {
+            ans *= i;
+        }
+
+        System.out.println(ans);
+    }
+}
