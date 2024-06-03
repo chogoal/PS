0c00682406fdbf76a617b42402f326113ec12e93
chogoal <goal961@naver.com>
2024-06-04 02:26:57 +0900

    [BOJ] A+B - 7

---
+++ java/BOJ_B5_11021.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B5_11021 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            sb.append("Case #").append(t).append(": ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A + B).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
