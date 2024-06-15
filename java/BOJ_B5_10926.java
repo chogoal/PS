57e47f549ff80d8c8967baace4771f23ade90661
chogoal <goal961@naver.com>
2024-06-15 23:36:29 +0900

    [BOJ] ??!

---
+++ java/BOJ_B5_10926.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B5_10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String id = br.readLine();
+
+        System.out.println(id + "??!");
+    }
+}
