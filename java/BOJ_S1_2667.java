4205dcb2cf401b9bf225c1b8c4fd91becc005721
chogoal <goal961@naver.com>
2024-06-26 17:25:14 +0900

    [BOJ] 단지번호붙이기

--- java/BOJ_S1_2667.java
+++ java/BOJ_S1_2667.java
@@ -2,16 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class BOJ_S1_2667 {
 
     static int N;
     static int[][] map;
-    static int[] dx = { 0, 1, 0, -1 };
-    static int[] dy = { 1, 0, -1, 0 };
-    static int count;
+    static boolean[][] visited;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static int house;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,46 +20,48 @@ public class BOJ_S1_2667 {
 
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
+        visited = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        ArrayList<Integer> danji = new ArrayList<Integer>();
+        int cnt = 0;
+        List<Integer> houseCnt = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1) {
-                    count = 1;
-                    dfs(new int[] { i, j });
-                    danji.add(count);
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    cnt++;
+
+                    house = 0;
+                    dfs(i, j);
+                    houseCnt.add(house);
                 }
             }
         }
 
-        Collections.sort(danji);
+        Collections.sort(houseCnt);
 
-        sb.append(danji.size()).append("\n");
-        for (int i = 0; i < danji.size(); i++) {
-            sb.append(danji.get(i)).append("\n");
-        }
+        sb.append(cnt).append("\n");
+        for (int h : houseCnt) sb.append(h).append("\n");
         System.out.println(sb.toString());
     }
 
-    private static void dfs(int[] cur) {
+    private static void dfs(int x, int y) {
 
-        int curX = cur[0];
-        int curY = cur[1];
-        map[curX][curY] = -1;
+        visited[x][y] = true;
+        house++;
 
         for (int d = 0; d < 4; d++) {
-            int nextX = curX + dx[d];
-            int nextY = curY + dy[d];
-            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && map[nextX][nextY] == 1) {
-                count++;
-                dfs(new int[] { nextX, nextY });
-            }
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) continue;
+
+            if (map[nx][ny] == 1) dfs(nx, ny);
         }
     }
 }
