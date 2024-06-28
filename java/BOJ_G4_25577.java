345d3a34e10d23adc197717eba8dbc0996ef345b
chogoal <goal961@naver.com>
2024-06-29 00:20:40 +0900

    [BOJ] 섞기 수열

---
+++ java/BOJ_G4_25577.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_G4_25577 {
+
+    static int N;
+    static int[] order;
+    static boolean[] visited, done;
+    static int size;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        order = new int[N + 1];
+        visited = new boolean[N + 1];
+        done = new boolean[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            order[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Set<Integer> sizeSet = new HashSet<>();
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                size = 1;
+                dfs(i, 1);
+                sizeSet.add(size);
+            }
+        }
+
+        List<Integer> sizeList = new ArrayList<>(sizeSet);
+        Collections.sort(sizeList, Collections.reverseOrder());
+
+        long lcm = sizeList.get(0);
+        for (int i = 1; i < sizeList.size(); i++) {
+            int n = sizeList.get(i);
+            lcm = lcm * n / gcd(lcm, n);
+        }
+
+        System.out.println(lcm);
+    }
+
+    private static void dfs(int idx, int cnt) {
+
+        visited[idx] = true;
+
+        int next = order[idx];
+        if (!visited[next]) {
+            dfs(next, cnt + 1);
+        } else if (!done[next]) {
+            size = cnt;
+        }
+
+        done[idx] = true;
+    }
+
+    // lcm = a * b / gcd(a, b);
+    private static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
