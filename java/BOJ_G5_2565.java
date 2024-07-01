351eb733a42c5ca6c1360de22bdb4d808a3b186a
chogoal <goal961@naver.com>
2024-07-01 17:48:53 +0900

    [BOJ] 전깃줄

---
+++ java/BOJ_G5_2565.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_G5_2565 {
+
+    static class Line implements Comparable<Line> {
+        int a;
+        int b;
+
+        public Line(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Line o) {
+            return this.a - o.a;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Line[] lines = new Line[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            lines[i] = new Line(a, b);
+        }
+
+        Arrays.sort(lines);
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (lines[i].b > lines[j].b) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N - max);
+    }
+}
