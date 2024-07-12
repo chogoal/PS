cbac276a78db073e5ff9d31beb12814f217f5fb2
chogoal <goal961@naver.com>
2024-07-12 09:37:32 +0900

    [BOJ] 오큰수

--- java/BOJ_G4_17298.java
+++ java/BOJ_G4_17298.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_G4_17298 {
 
@@ -11,43 +8,28 @@ public class BOJ_G4_17298 {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
 
-        int[] array = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Stack<Integer> stack = new Stack<>();
-        int[] ans = new int[N];
-
-ex:     for (int i = N - 1; i >= 0; i--) {
-            if (stack.empty()) {
-                ans[i] = -1;
-                stack.push(array[i]);
-                continue;
-            }
-
-            int top = stack.peek();
-            while (top <= array[i]) {
-                stack.pop();
-                if (stack.empty()) {
-                    ans[i] = -1;
-                    stack.push(array[i]);
-                    continue ex;
-                } else {
-                    top = stack.peek();
-                }
-            }
-
-            if (top > array[i]) {
-                ans[i] = top;
-                stack.push(array[i]);
-            }
-        }
 
         for (int i = 0; i < N; i++) {
-            sb.append(ans[i]).append(" ");
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int a : arr) {
+            sb.append(a).append(" ");
         }
         System.out.println(sb.toString());
     }
