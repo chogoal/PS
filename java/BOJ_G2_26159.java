acdcc7566615d1c6c235fee3e1a557e3c53b79de
chogoal <goal961@naver.com>
2024-07-13 19:04:14 +0900

    [BOJ] 트리와 수열

--- java/BOJ_G2_26159.java
+++ java/BOJ_G2_26159.java
@@ -7,7 +7,7 @@ public class BOJ_G2_26159 {
 
     static int N;
     static List<Integer>[] graph;
-    static int[] arr, subtree, parent;
+    static int[] arr, subtree;
     static long[] used;
     static final int DIVIDE = (int) (Math.pow(10, 9) + 7);
 
@@ -16,9 +16,8 @@ public class BOJ_G2_26159 {
 
         N = Integer.parseInt(br.readLine());
         graph = new List[N + 1];
-        arr = new int[N];
+        arr = new int[N - 1];
         subtree = new int[N + 1];
-        parent = new int[N + 1];
         used = new long[N + 1];
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
@@ -35,7 +34,7 @@ public class BOJ_G2_26159 {
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N - 1; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -45,8 +44,8 @@ public class BOJ_G2_26159 {
         Arrays.sort(arr);
 
         long sum = 0;
-        for (int i = 2, j = N - 1; i <= N && j >= 1; i++, j--) {
-            sum += used[i] * arr[j] % DIVIDE;
+        for (int i = 2; i <= N; i++) {
+            sum += used[i] * arr[N - i] % DIVIDE;
         }
 
         System.out.println(sum % DIVIDE);
@@ -56,7 +55,6 @@ public class BOJ_G2_26159 {
 
         for (int next : graph[cur]) {
             if (next == prev) continue;
-            parent[next] = cur;
             dfs(next, cur);
             subtree[cur] += subtree[next];
         }
